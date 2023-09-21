bf2d7824418653f4d18efceea4369e237fb8414b
moukouba <m_moukouba@yahoo.fr>
2023-09-21 10:38:20 +0200

    data ingestion to be continued

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r", encoding="utf-8") as f:
 
 __version__ = "0.0.0"
 
-REPO_NAME = "Chicken-Disease-Classification--Project"
+REPO_NAME = "Chicken-Disease-Classification-Project"
 AUTHOR_USER_NAME = "Moukouba"
 SRC_REPO = "cnnClassifier"
 AUTHOR_EMAIL = "m_moukouba@yahoo.fr"
